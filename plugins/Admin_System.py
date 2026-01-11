2ff970fe80d4fbf5c5609a167f4761b69c52bb42
ʀɪᴏ sʜɪɴ <antmangktifjfjtjfjft@gmail.com>
2026-01-11 06:33:58 +0530

    Create Admin_System.py

---
+++ plugins/Admin_System.py
@@ -0,0 +1,57 @@
+# plugins/Admin_System.py
+
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from config import Config 
+from database.database import add_admin, remove_admin, get_all_admins
+import asyncio
+
+OWNER_ID = Config.OWNER_ID  # Owner check
+
+# ➕ Add Admin
+@Client.on_message(filters.command("add_admin") & filters.private)
+async def add_admin_handler(client, message):
+    if message.from_user.id != OWNER_ID:
+        return await message.reply("<code>Oɴʟʏ ᴛʜᴇ ᴏᴡɴᴇʀ ᴄᴀɴ ᴅᴏ ᴛʜɪꜱ, ʙᴀʙʏ...</code>")
+
+    try:
+        user_id = int(message.command[1])
+    except (IndexError, ValueError):
+        return await message.reply("<code>Gɪᴠᴇ ᴀ ᴘʀᴏᴘᴇʀ ᴜꜱᴇʀ ɪᴅ, ʜᴜɴᴛᴇʀ</code>")
+
+    add_admin(user_id)
+    await message.reply(
+        f"<code>➥ Aᴅᴅᴇᴅ {user_id} ᴛᴏ ᴍʏ ʟᴏʏᴀʟ ᴀᴅᴍɪɴꜱ ♨️</code>"
+    )
+
+# ➖ Remove Admin
+@Client.on_message(filters.command("remove_admin") & filters.private)
+async def remove_admin_handler(client, message):
+    if message.from_user.id != OWNER_ID:
+        return await message.reply("<code>Yᴏᴜ ᴄᴀɴ'ᴛ ʀᴇᴍᴏᴠᴇ ᴘᴏᴡᴇʀ, ʜᴏɴᴇʏ...</code>")
+
+    try:
+        user_id = int(message.command[1])
+    except (IndexError, ValueError):
+        return await message.reply("<code>Pᴜᴛ ᴀ ᴠᴀʟɪᴅ ᴜꜱᴇʀ ɪᴅ, ᴅᴏɴ'ᴛ ᴍᴇss ᴡɪᴛʜ ᴍᴇ</code>")
+
+    remove_admin(user_id)
+    await message.reply(
+        f"<code>➥ {user_id} ɪs ɴᴏ ʟᴏɴɢᴇʀ ᴏɴ ᴛʜᴇ ᴛʜʀᴏɴᴇ 💔</code>"
+    )
+
+# 📜 List Admins
+@Client.on_message(filters.command("admins_list") & filters.private)
+async def admins_list_handler(client, message):
+    if message.from_user.id != OWNER_ID:
+        return await message.reply("<code>Oɴʟʏ ᴛʜᴇ ᴏɴᴇ ᴡʜᴏ ʀᴜʟᴇꜱ ᴄᴀɴ ʟɪꜱᴛ ᴛʜᴇ ʟᴏʏᴀʟ</code>")
+
+    admins = get_all_admins()
+    if not admins:
+        return await message.reply("<code>Nᴏ ᴀᴅᴍɪɴꜱ ʏᴇᴛ, ᴍʏ ʟᴏʀᴅ</code>")
+
+    admins_text = "\n".join([f"➥ <code>{uid}</code>" for uid in admins])
+    await message.reply(
+        f"<b>⚙️ Aᴄᴛɪᴠᴇ Aᴅᴍɪɴꜱ:</b>\n{admins_text}",
+        parse_mode="html"
+  )
